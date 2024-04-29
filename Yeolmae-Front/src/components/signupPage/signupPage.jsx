6e50278760d87dea3d5efd05116ce19b7f6d0a66
ooylf <118517380+yoolf@users.noreply.github.com>
2024-04-29 23:08:49 +0900

    refactor: Wrapper 컴포넌트 파일 분리

--- Yeolmae-Front/src/components/signupPage/signupPage.jsx
+++ Yeolmae-Front/src/components/signupPage/signupPage.jsx
@@ -1,23 +1,8 @@
 import styled from 'styled-components';
-import Header from '../common/header';
-import Button from '../common/button';
-import InputField from '../common/inputField';
+import Button from '../common/Button';
+import Wrapper from '../common/Wrapper';
+import InputField from '../common/InputField';
 
-const Wrapper = styled.div`
-  background-color: white;
-  display: flex;
-  gap: 10px;
-  flex-direction: column;
-  justify-content: center;
-  width: 70%;
-  margin: 50px auto;
-  padding: 0px 30px;
-  min-height: 70vh;
-  max-width: 50vh;
-  height: 60%;
-  box-shadow: rgba(100, 100, 100, 0.2) 0px 0px 29px 0px;
-  border-radius: 30px;
-`;
 const InputWrapper = styled.div`
   display: flex;
   min-height: 40vh;
@@ -30,14 +15,11 @@ const Title = styled.h2`
 
 function Signup() {
   return (
-    <>
-      <Header />
-      <Wrapper>
-        <Title>회원가입하기</Title>
-        <InputWrapper>인풋필드</InputWrapper>
-        <Button text="가입하기" />
-      </Wrapper>
-    </>
+    <Wrapper>
+      <Title>회원가입하기</Title>
+      <InputWrapper>인풋필드</InputWrapper>
+      <Button text="가입하기" />
+    </Wrapper>
   );
 }
 
