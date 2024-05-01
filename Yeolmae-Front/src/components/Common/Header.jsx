e8b191c24ca9e22cc25838f11ceb28d01e879bf4
ooylf <118517380+yoolf@users.noreply.github.com>
2024-05-01 22:50:33 +0900

    feat: 폰트 적용 후 스타일 변경

--- Yeolmae-Front/src/components/Common/Header.jsx
+++ Yeolmae-Front/src/components/Common/Header.jsx
@@ -1,15 +1,21 @@
 import { Link } from 'react-router-dom';
 import './Header.css';
+import styled from 'styled-components';
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: white;
+`;
 
 function Header() {
   return (
     <header className="Header">
       <div className="headerLeft">
-        <Link to="/">로고</Link>
+        <StyledLink to="/">로고</StyledLink>
       </div>
       <div className="headerCenter" />
       <div className="headerRight">
-        <Link to="/loginpage">로그인</Link>
+        <StyledLink to="/loginpage">로그인</StyledLink>
       </div>
     </header>
   );
